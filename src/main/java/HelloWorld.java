b3e73ee87c19fa2a5610475f6aa061de79323921
CHERRYPICK\Cherry <1>
2025-12-25 12:57:28 +0800

    finaltest

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+
+        for (WorkBarrie workBarrie : WorkBarrie.values()) {
+            System.out.println(workBarrie); 
+        }
+
     }
 }
